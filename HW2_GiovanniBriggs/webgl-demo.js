6058d00a98043fee6e337abcc4bef80efc3028f1
Giovanni Briggs <gbriggs2012@gmail.com>
2015-10-22 20:16:20 -0700

    More edits

--- HW2_GiovanniBriggs/webgl-demo.js
+++ HW2_GiovanniBriggs/webgl-demo.js
@@ -33,7 +33,7 @@ function start() {
   // Only continue if WebGL is available and working
 
   if (gl) {
-    gl.clearColor(1.0, 1.0, 1.0, 1.0);  // Clear to black, fully opaque
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
     gl.clearDepth(1.0);                 // Clear everything
     gl.enable(gl.DEPTH_TEST);           // Enable depth testing
     gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
@@ -94,45 +94,6 @@ function initBuffers() {
 
   gl.bindBuffer(gl.ARRAY_BUFFER, cubeVerticesBuffer);
 
-  // Now create an array of vertices for the cube.
-
-  /*var vertices = [
-    // Front face
-    -1.0, -1.0,  1.0,
-     1.0, -1.0,  1.0,
-     1.0,  1.0,  1.0,
-    -1.0,  1.0,  1.0,
-
-    // Back face
-    -1.0, -1.0, -1.0,
-    -1.0,  1.0, -1.0,
-     1.0,  1.0, -1.0,
-     1.0, -1.0, -1.0,
-
-    // Top face
-    -1.0,  1.0, -1.0,
-    -1.0,  1.0,  1.0,
-     1.0,  1.0,  1.0,
-     1.0,  1.0, -1.0,
-
-    // Bottom face
-    -1.0, -1.0, -1.0,
-     1.0, -1.0, -1.0,
-     1.0, -1.0,  1.0,
-    -1.0, -1.0,  1.0,
-
-    // Right face
-     1.0, -1.0, -1.0,
-     1.0,  1.0, -1.0,
-     1.0,  1.0,  1.0,
-     1.0, -1.0,  1.0,
-
-    // Left face
-    -1.0, -1.0, -1.0,
-    -1.0, -1.0,  1.0,
-    -1.0,  1.0,  1.0,
-    -1.0,  1.0, -1.0
-  ];*/
   var vertices = verts;
   num_points = vertices.length;
   // Now pass the list of vertices into WebGL to build the shape. We
@@ -153,21 +114,9 @@ function initBuffers() {
     [1.0,  0.0,  1.0,  1.0]     // Left face: purple
   ];
 
-  // Convert the array of colors into a table for all the vertices.
-
-  var generatedColors = [];
-
-  /*for (j=0; j<6; j++) {
-    var c = colors[j];
-
-    // Repeat each color four times for the four vertices of the face
-
-    for (var i=0; i<4; i++) {
-      generatedColors = generatedColors.concat(c);
-    }
-  }*/
   //loop through and create a list of colors for each vertex
-  for (j=0; j<verts.length; j+=100) {
+  var generatedColors = [];
+  for (j=0; j<verts.length; j+=6) {
     var r = Math.floor(Math.random() * (colors.length - 0 + 1) + 0);
     var c = colors[r];
     for(h = 0; h < 100; h++) {
@@ -186,18 +135,7 @@ function initBuffers() {
   cubeVerticesIndexBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVerticesIndexBuffer);
 
-  // This array defines each face as two triangles, using the
-  // indices into the vertex array to specify each triangle's
-  // position.
-
-  /*var cubeVertexIndices = [
-    0,  1,  2,      0,  2,  3,    // front
-    4,  5,  6,      4,  6,  7,    // back
-    8,  9,  10,     8,  10, 11,   // top
-    12, 13, 14,     12, 14, 15,   // bottom
-    16, 17, 18,     16, 18, 19,   // right
-    20, 21, 22,     20, 22, 23    // left
-  ]*/
+  // Flattened array of indices
   var cubeVertexIndices = indices;
 
   // Now send the element array to GL
@@ -255,7 +193,7 @@ function drawScene() {
 
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVerticesIndexBuffer);
   setMatrixUniforms();
-  gl.drawElements(gl.TRIANGLES, num_points, gl.UNSIGNED_SHORT, 0);
+  gl.drawElements(gl.LINE_STRIP, num_points, gl.UNSIGNED_SHORT, 0);
 
   // Restore the original matrix
 
@@ -267,16 +205,16 @@ function drawScene() {
   if (lastCubeUpdateTime) {
     var delta = currentTime - lastCubeUpdateTime;
 
-    cubeRotation += (30 * delta) / 1000.0;
-    cubeXOffset += xIncValue * ((30 * delta) / 1000.0);
-    cubeYOffset += yIncValue * ((30 * delta) / 1000.0);
-    cubeZOffset += zIncValue * ((30 * delta) / 1000.0);
+    //cubeRotation += (30 * delta) / 1000.0;
+    /*cubeXOffset += xIncValue * ((3 * delta) / 1000.0);
+    cubeYOffset += yIncValue * ((3 * delta) / 1000.0);
+    cubeZOffset += zIncValue * ((3 * delta) / 1000.0);*/
 
-    if (Math.abs(cubeYOffset) > 2.5) {
+    /*if (Math.abs(cubeYOffset) > 2.5) {
       xIncValue = -xIncValue;
       yIncValue = -yIncValue;
       zIncValue = -zIncValue;
-    }
+    }*/
   }
 
   lastCubeUpdateTime = currentTime;
